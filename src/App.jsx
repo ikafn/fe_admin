c7b6d0003c6ab4963ea6de6729054b54a0e2a915
ika <ikafauziahnadila@gmail.com>
2023-11-26 21:38:45 +0700

    [admin kelola kelas] create admin kelola kelas page: ika

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { AdminDashboard, AdminLogin, Register } from "./page"
+import { AdminDashboard, AdminKelas, AdminLogin, Register } from "./page"
 import Home from "./page/Home"
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
         <Route path="/register" element={<Register/>}/>
         <Route path="/login/admin" element={<AdminLogin/>}/>
         <Route path="/admin/dashboard" element={<AdminDashboard />}/>
+        <Route path="/admin/kelas" element={<AdminKelas />}/>
+
 
       </Routes>
     </BrowserRouter>
--- src/page/index.jsx
+++ src/page/index.jsx
@@ -2,8 +2,10 @@ import Register from "./Register";
 import { Button } from "../component";
 import AdminLogin from "./AdminLogin";
 import AdminDashboard from "./AdminDashboard";
+import AdminKelas from "./AdminKelas";
 
 export {Register,
     Button,
     AdminLogin,
-    AdminDashboard}
+    AdminDashboard,
+    AdminKelas}
